a04b9491f710ce4ac8ef93e0fa5d1774432a057d
hecl <zhejiang@allinpay.com>
2020-08-28 16:56:37 +0800

    初始提交

---
+++ src/main/java/com/venus/tspro/TsproApplication.java
@@ -0,0 +1,13 @@
+package com.venus.tspro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsproApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TsproApplication.class, args);
+    }
+
+}
