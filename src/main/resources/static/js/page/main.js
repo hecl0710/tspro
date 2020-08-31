21d9a3da6eb6e359a37ed564f938f96ed53a167f
hecl <zhejiang@allinpay.com>
2020-08-31 16:51:28 +0800

    优化首页

--- src/main/resources/static/js/page/main.js
+++ src/main/resources/static/js/page/main.js
@@ -46,7 +46,7 @@ layui.use(['layer', 'miniTab','echarts'], function () {
             trigger: 'axis'
         },
         legend: {
-            data:['邮件营销','联盟广告','视频广告','直接访问','搜索引擎']
+            data:['充值金额','结算笔数','用户数']
         },
         grid: {
             left: '3%',
@@ -69,29 +69,19 @@ layui.use(['layer', 'miniTab','echarts'], function () {
         },
         series: [
             {
-                name:'邮件营销',
+                name:'充值金额',
                 type:'line',
                 data:[120, 132, 101, 134, 90, 230, 210]
             },
             {
-                name:'联盟广告',
+                name:'结算笔数',
                 type:'line',
                 data:[220, 182, 191, 234, 290, 330, 310]
             },
             {
-                name:'视频广告',
+                name:'用户数',
                 type:'line',
                 data:[150, 232, 201, 154, 190, 330, 410]
-            },
-            {
-                name:'直接访问',
-                type:'line',
-                data:[320, 332, 301, 334, 390, 330, 320]
-            },
-            {
-                name:'搜索引擎',
-                type:'line',
-                data:[820, 932, 901, 934, 1290, 1330, 1320]
             }
         ]
     };
